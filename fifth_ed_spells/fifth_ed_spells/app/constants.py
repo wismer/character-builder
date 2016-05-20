d840de12832ad59ff154756d9b764ac3e8bf5c30
Matt Long <matthewhl@gmail.com>
2016-05-19 22:40:06 -0400

    add models

---
+++ fifth_ed_spells/fifth_ed_spells/app/constants.py
@@ -0,0 +1,10 @@
+ARMOR_VALUES = [i for i in range(0, 30)]
+ARMOR_TYPES = ['light', 'medium', 'heavy', 'small shield', 'shield', 'large shield']
+
+DIE_COUNT = ARMOR_VALUES[0:6]
+DIE_CHOICES = [4, 6, 8, 10, 12, 20, 100]
+
+SHORT_RANGE = [None, 5, 20, 30, 80, 100, 150]
+LONG_RANGE = [None, 15, 60, 120, 320, 400, 600]
+
+MUNDANE_DAMAGE_TYPES = ['bludgeoning', 'piercing', 'slashing']
