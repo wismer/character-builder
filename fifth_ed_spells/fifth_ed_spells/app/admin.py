8ea97024163c6c815691e294f132bd638865cc08
Matt Long <matthewhl@gmail.com>
2016-05-20 01:26:10 -0400

    go to sleep matt

--- fifth_ed_spells/fifth_ed_spells/app/admin.py
+++ fifth_ed_spells/fifth_ed_spells/app/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
-from .models import ParentRace, SubRace, RacialTrait
+from .models import (
+    ParentRace,
+    SubRace,
+    RacialTrait,
+    Item,
+    Trait,
+    TraitProperty,
+    Weapon,
+    Armor,
+)
 
+
+class TraitPropertyAdmin(admin.ModelAdmin):
+    pass
+
+
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+
+class ArmorAdmin(admin.ModelAdmin):
+    pass
+
+
+class WeaponAdmin(admin.ModelAdmin):
+    pass
 
 
 class RacialTraitAdminInline(admin.TabularInline):
@@ -15,6 +39,10 @@ class RacialTraitAdmin(admin.ModelAdmin):
     pass
 
 
+class TraitAdmin(admin.ModelAdmin):
+    pass
+
+
 class SubRaceAdmin(admin.ModelAdmin):
     inlines = [RacialTraitAdminInline]
 
@@ -22,3 +50,8 @@ class SubRaceAdmin(admin.ModelAdmin):
 admin.site.register(ParentRace, ParentRaceAdmin)
 admin.site.register(SubRace, SubRaceAdmin)
 admin.site.register(RacialTrait, RacialTraitAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Armor, ArmorAdmin)
+admin.site.register(Weapon, WeaponAdmin)
+admin.site.register(Trait, TraitAdmin)
+admin.site.register(TraitProperty, TraitPropertyAdmin)
