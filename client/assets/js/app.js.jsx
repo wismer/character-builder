9e7f9984a27dfe4d6f33f990dc4b48b612e2d357
Matt Long <matthewhl@gmail.com>
2016-05-30 22:38:41 -0400

    clearing some space

--- client/assets/js/app.js.jsx
+++ client/assets/js/app.js.jsx
@@ -4,27 +4,16 @@ import { RaceList, RaceChoice } from './components/races/race-list';
 import { readableAttributes, convertScore } from './util/constants';
 import { retrieve } from './util/adapter';
 import Dashboard from './components/dashboard/dashboard';
-import { Weapon, Armor, Item, Player } from './classes/main';
+import { Weapon, Armor, Item, Player, PlayerRace } from './classes/main';
 import SkillActions from './mixins/skill-actions';
-let renderCount = 0;
+
+
 const STEPS = [
     { component: RaceList }
 ]
-const sampleData = {
-  perks: [
-    {name: 'dwarven toughness', effect: '+1 hp per level'},
-    {name: 'dwarven resilience', effect: 'poison resist'}
-  ],
-
-  skills: [
-    { name: 'medicine', proficient: false },
-  ]
-}
 
 let App = React.createClass({
   getInitialState() {
-    var skills = this.props.items.skills;
-    skills.forEach(skill => skill.isActive = false);
     return {
       abilityScores: [8, 8, 8, 8, 8, 8, 8],
       armor: [],
@@ -37,7 +26,6 @@ let App = React.createClass({
       hasDarkvision: false,
       proficiencyBonus: 0,
       savingThrowAbilities: [],
-      trainedSkills: skills, // { name: string, isProficient: boolean }
       hp: 0,
       level: 1,
       race: null,
@@ -49,21 +37,31 @@ let App = React.createClass({
     };
   },
 
+  updateSelection(evt, race) {
+    if (evt === 'select') {
+      this.setState({ race });
+    }
+  },
+
   _renderCurrentStep() {
     var step = STEPS[this.state._currentStep];
     var races = this.props.races;
-    return React.createElement(step.component, {races});
+    var updateSelection = this.updateSelection;
+    return React.createElement(step.component, {races, updateSelection});
   },
 
   render() {
-    var { weapons, armor, skills } = this.props.items;
-    var trainedSkills = this.state.trainedSkills;
-    var abilities = this.state.abilityScores.map(convertScore);
+    var { weapons, armor, skills } = this.props.items,
+        abilities = this.state.abilityScores.map(convertScore),
+        race;
+    if (this.state.race) {
+      race = new
+    }
 
     return (
       <div className='primary-node'>
         <div id='current-view'>
-
+          {this._renderCurrentStep()}
         </div>
         <Dashboard skills={skills} abilities={abilities} />
       </div>
--- client/assets/js/classes/main.js
+++ client/assets/js/classes/main.js
@@ -1,5 +1,5 @@
 import { Item, Weapon, Armor } from './item';
 import Player from './player';
+import PlayerRace from './race';
 
-
-export { Player, Item, Armor, Weapon };
+export { Player, Item, Armor, Weapon, PlayerRace };
