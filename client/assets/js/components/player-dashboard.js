6c9926d74beb1ed2b6f5f0057b4a8f2fec74c831
Matt Long <matthewhl@gmail.com>
2016-07-12 14:38:38 -0400

    redo dashboard

--- client/assets/js/components/player-dashboard.js
+++ client/assets/js/components/player-dashboard.js
@@ -1,15 +1,59 @@
 import React from 'react';
-import PrimaryAttributes from './primary-stats';
+import { classes } from '../utils/helpers';
 
-export default React.createClass({
+
+class Step extends React.Component {
+  get classNames() {
+    return { 'step': true, 'active-step': this.isCurrentStep };
+  }
+
+  get isCurrentStep() {
+    return this.props.activeStep == this.props.stepNumber;
+  }
+
+  get href() {
+    return this.props.href;
+  }
+
+  get label() {
+    return this.props.label;
+  }
+
   render() {
+    return <a className={classes(this.classNames)} href={this.href}>{this.label}</a>;
+  }
+}
+
+Step.propTypes = {
+  label: React.propTypes.string,
+  href: React.propTypes.string,
+  activeStep: React.propTypes.number,
+  stepNumber: React.propTypes.number
+};
+
+class CharacterCreationState extends React.Component {
+  render() {
+    var steps = this.props.steps.map(step => <Step step={step} />);
     return (
-      <div className='user-panel'>
-        <div className='attribute-panel-heading'>
-          Attributes
-        </div>
-        <PrimaryAttributes attributes={this.props.attributes} />
-      </div>
+      <nav id='navigation'>
+        <ul>
+          {steps}
+        </ul>
+      </nav>
     );
   }
-});
+}
+
+CharacterCreationState.propTypes = {
+  step: React.propTypes.number.isRequired,
+  steps: React.propTypes.array
+};
+
+CharacterCreationState.defaultProps = {
+  step: 0,
+  steps: [
+    { label: 'Choose a Race', href: '/pick-race' },
+    { label: 'Adjust your Ability Scores', href: '/pick-scores' },
+    { label: 'Pick your Character Class', href: '/pick-class' }
+  ]
+};
